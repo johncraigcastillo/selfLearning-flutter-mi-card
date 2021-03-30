c096907f73841fe2ac4720e505d0b6076afbbd74
johncastillo <johncraigcastillo@gmail.com>
2021-03-30 05:12:48 -0500

    mi card init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            //margin affects the outside of the widget
+            margin: EdgeInsets.only(left: 30),
+            //padding affects the inside (the child) of the widget
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
