7da342a8a46bb95d8254696856a11762c22c6c58
johncastillo <johncraigcastillo@gmail.com>
2021-04-01 22:25:05 -0500

    learned Card, and ListTile, Padding Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,23 +15,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  foregroundImage: AssetImage('images/image.jpg'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                foregroundImage: AssetImage('images/image.jpg'),
+              ),
+              Text(
+                'John Castillo',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
                 ),
-                Text(
-                  'John Castillo',
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontFamily: 'Pacifico',
-                  ),
-                ),
-                Text(
+              ),
+              Padding(
+                padding: const EdgeInsets.all(3),
+                child: Text(
                   'FLUTTER DEVELOPER',
                   style: TextStyle(
                     color: Colors.teal[100],
@@ -40,37 +42,56 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        size: 30,
-                        color: Colors.teal[800],
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '(555)453-6592',
-                        style: TextStyle(
-                          fontSize: 23,
-                          fontFamily: 'Source Sans Pro',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.teal[800],
-                          letterSpacing: 2.5,
-                        ),
-                      ),
-                    ],
+              ),
+              SizedBox(
+                height: 15,
+                width: 195,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                elevation: 3,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[800],
+                    size: 30,
+                  ),
+                  title: Text(
+                    '(832)741-5469',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal[800],
+                      letterSpacing: 2,
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              Card(
+                elevation: 3,
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[800],
+                    size: 30,
+                  ),
+                  title: Text(
+                    'johncraigcastillo@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[800],
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
